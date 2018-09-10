deb584f5b79f1d203e00681d8bc791a32f60e069
Oscar Correa <oscarcorreag@gmail.com>
2018-09-11 06:51:16 +1000

    Test second commit on branch-test-2

--- satellite/views.py
+++ satellite/views.py
@@ -148,6 +148,7 @@ def filter_by_time(request, param_name):
 def branch_test_1(request):
     if request:
         print request.GET.get['year']
+        print request.GET.get['page']
         return True
     return False
 
